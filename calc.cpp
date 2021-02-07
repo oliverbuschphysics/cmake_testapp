0c41765b36fbef0e5e28a982bbf8efa814fdf946
oli <oliver.busch.92@gmx.de>
2021-02-07 20:53:39 +0100

    Initial commit

---
+++ calc.cpp
@@ -0,0 +1,4 @@
+//
+// Created by oli on 06.02.21.
+//
+
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// C++ style - single line comment \
+that continues due to backslash to next line
+/*
+ * C-style
+ * Multi-line comment
+ * */
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
